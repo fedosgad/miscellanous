1e33b21581ad444a8dc9efd2f13c750c7ef9fe14
fedosgad <fedosgad@gmail.com>
2018-11-14 14:30:16 +0300

    Finished polynomial class

--- mipt/5_term/interpolation/interpolate.cpp
+++ mipt/5_term/interpolation/interpolate.cpp
@@ -9,9 +9,11 @@ using namespace std;
 class polynomial {
 private:
 	int n;	//polynome degree
-	vector<FTYPE> coef;	//coefficients, power highest power first
+	vector<FTYPE> coef;	//coefficients, highest power first
 
 public:
+	polynomial() {}
+
 	polynomial(const int _n) {
 		n = _n;
 		coef.resize(n + 1);
@@ -33,18 +35,31 @@ public:
 		return n;
 	}
 
+	polynomial deriv() {
+		polynomial result(n - 1);
+
+		for(int i = 0; i < n; i++)
+			result.set_coef(i, coef[i + 1]*(i + 1));
+
+		return result;
+	}
 
 };
 
 int main() {
 	FTYPE x;
-	polynomial poly(0);
+	polynomial poly(2);
 
-	poly.set_coef(0, 1.65);
+	poly.set_coef(0, 1);
+	poly.set_coef(1, 1);
+	poly.set_coef(2, 1);
+
+	polynomial der_poly = poly.deriv();
 
 	cin >> x;
 
 	cout << poly.val(x) << endl;
+	cout << der_poly.val(x) << endl;
 
 	return 0;
 }
