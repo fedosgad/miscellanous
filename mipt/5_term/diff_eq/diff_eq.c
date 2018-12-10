b69276ae19bc0caba6544cfe71c7f6698899675c
fedosgad <fedosgad@gmail.com>
2018-12-10 21:06:45 +0300

    Wrote the differential equation solver

---
+++ mipt/5_term/diff_eq/diff_eq.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <math.h>
+#include <float.h>
+
+#define FTYPE float
+
+#define LEFT_BORDER 1.0
+#define RIGHT_BORDER 2.0
+#define EPSILON 1e-4
+#define Y_LB 1.0	//Y at left border
+#define K 4
+#define H 0.1
+
+FTYPE f(FTYPE x, FTYPE y);
+
+int main() {
+	FTYPE h, cur_x, cur_y;
+	FTYPE f1, f2, f3, f4;
+	FTYPE step;
+	FTYPE diff;
+
+	FTYPE cur_y_vals[11];
+	FTYPE prev_y_vals[11];
+
+	int i, first_time, iteration, flag;
+
+	step = (RIGHT_BORDER - LEFT_BORDER)/10;
+	h = H;
+	diff = 0;
+
+	first_time = 1;
+	flag = 0;
+	iteration = 0;
+
+	while(!flag) {
+		printf("Iteration %i, h = %e, ", iteration, h);
+
+		if(!first_time) {	//save previous results
+			for(i = 0; i < 11; i++)
+				prev_y_vals[i] = cur_y_vals[i];
+		}
+
+		cur_y_vals[0] = Y_LB;
+		cur_y = Y_LB;
+		cur_x = LEFT_BORDER;
+
+		while(cur_x < RIGHT_BORDER) {	//obtain values
+			f1 = f(cur_x, cur_y);	//calculate next Yn
+			f2 = f(cur_x + h/2, cur_y + h/2*f1);
+			f3 = f(cur_x + h/2, cur_y + h/2*f2);
+			f4 = f(cur_x + h, cur_y + h*f3);
+
+			cur_x += h;
+			cur_y = cur_y + h/6*(f1 + 2*f2 + 2*f3 + f4);
+
+			for(i = 1; i < 11; i++) {	//save value if needed
+				if(fabs(cur_x - (LEFT_BORDER + i*step)) < h/2) {
+					cur_y_vals[i] = cur_y;
+					//~ printf("Saved %e as y[%i]\n", cur_y, i);
+					break;
+				}
+			}
+		}
+
+		if(!first_time) {	//check if enough
+			diff = fabs(prev_y_vals[0] - cur_y_vals[0]);
+			for(i = 0; i < 11; i++) {
+				if(fabs(prev_y_vals[i] - cur_y_vals[i]) > diff)
+					diff = fabs(prev_y_vals[i] - cur_y_vals[i]);
+			}
+
+			if(diff/(pow(2, K) - 1) < EPSILON)
+				flag = 1;
+		}
+
+		printf("diff = %e\n", diff);
+
+		first_time = 0;
+		iteration++;
+		h = h/2;
+	}
+
+	printf("\n");
+
+	printf("x:");
+	for(i = 0; i < 11; i++)
+		printf("\t%e", LEFT_BORDER + i*step);
+	printf("\n");
+
+	printf("y(2h):");
+	for(i = 0; i < 11; i++)
+		printf("\t%e", prev_y_vals[i]);
+	printf("\n");
+
+	printf("y(h):");
+	for(i = 0; i < 11; i++)
+		printf("\t%e", cur_y_vals[i]);
+	printf("\n");
+
+	printf("dy:");
+	for(i = 0; i < 11; i++)
+		printf("\t%e", fabs(prev_y_vals[i] - cur_y_vals[i]));
+	printf("\n");
+
+	return 0;
+}
+
+FTYPE f(FTYPE x, FTYPE y) {
+	return (2*pow(x, 3) + pow(x, 2) - pow(y, 2))/(2*pow(x, 2)*y);
+	//~ return x;
+}
+
--- mipt/5_term/interpolation/interpolate.cpp
+++ mipt/5_term/interpolation/interpolate.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+FTYPE f(pair< FTYPE, FTYPE > a, pair< FTYPE, FTYPE > b);
+
 class polynomial {
 private:
 	int n;	//polynome degree
@@ -82,11 +84,14 @@ public:
 };
 
 int main() {
-	FTYPE x;
+	FTYPE x, tmp, tmp_coef;
 	int n;
 
 	vector< pair< FTYPE, FTYPE > > data;	//initial data
 	vector< spline_segment > spline;	//resulting cubic spline
+	vector< int > counter;	//cycle counters for Newton poly's calculation
+	vector< FTYPE > div_diff;
+	vector< FTYPE > b_coef;
 
 	cout << "Amount of values: ";
 	cin >> n;
@@ -101,9 +106,89 @@ int main() {
 
 	polynomial newton_poly(n);
 
-	for(int i = 0; i < n; i++) {	//calculate Newton's poly's coefs
-		
+	counter.resize(n);
+	b_coef.resize(n);
+	div_diff.resize(n);
+
+	b_coef[0] = data[0].second;
+
+	for(int i = 0; i < n - 1; i++) {	//calculate divided diffs
+		if(i == 0) {
+			for(int j = 1; j < n - i; j++) {
+				div_diff[j - 1] = f(data[j - 1], data[j]);
+			}
+		}
+		else {
+			for(int j = 1; j < n - i; j++) {
+				div_diff[j - 1] = f( make_pair( data[j - 1].first, div_diff[j - 1] ), 
+							make_pair( data[j + i].first, div_diff[j - 1] )
+							);
+			}
+		}
+
+		b_coef[i + 1] = div_diff[0];
+
+	}
+
+	cout << "b_coefs:" << endl;
+	for(int i = 0; i < n - 1; i++)
+		cout << b_coef[i] << endl;
+	cout << endl;
+
+	int cur_count = n - 1;
+	tmp = 0;
+	tmp_coef = 0;
+
+	for(int i = 0; i < n; i++) {	//calculate poly's coefs
+		for(int j = 0; j < n - i; j++)
+				counter[j] = j;	//initial fill-in
+
+		bool flag = false;
+
+		tmp = data[ counter[0] ].first;
+		for(int k = 1; k < n - i; k++)
+			tmp *= data[ counter[k] ].first;
+		tmp_coef += tmp;
+
+		cur_count = n - 1 - i;
+
+		while( !flag ) {
+			while(1) {	//generate next combination
+				if(counter[cur_count] < i + cur_count) {
+					counter[cur_count]++;
+					for(int l = cur_count + 1; l < n - i; l++)
+						counter[l] = counter[l - 1] + 1;
+
+					cur_count = n - 1;
+					break;
+				}
+				else if((cur_count == 0) && (counter[0] == i)) {
+					flag = true;
+					break;
+				}
+				else {
+					cur_count--;
+				}
+			}
+
+			tmp = data[ counter[0] ].first;
+			for(int k = 1; k < n - i; k++)
+				tmp *= data[ counter[k] ].first;
+			tmp_coef += tmp;
+		}
+
+		newton_poly.set_coef(i, tmp_coef*b_coef[i]);
+
+		cout << tmp_coef << endl;
+
+		tmp_coef = 0;
+
 	}
 
 	return 0;
 }
+
+FTYPE f(pair< FTYPE, FTYPE > a, pair< FTYPE, FTYPE > b) {
+	return (b.second - a.second)/(b.first - a.first);
+}
+
